48988b1a5e364a50e539b8c6d254683b26a6cafc
genneko <genneko@users.noreply.github.com>
2017-12-28 17:28:37 +0900

    Add update support to prepare.sh.

--- prepare.sh
+++ prepare.sh
@@ -5,23 +5,46 @@
 prog=$(basename $0)
 basedir=$(dirname $(readlink -f $0))
 
+echoerr() {
+	echo "$@" >&2
+}
+
 targetdir=$1
 if [ -z "$targetdir" ]; then
-	echo "usage: $prog <foldername>"
+	echoerr "usage: $prog <foldername>"
 	exit 1
 fi
 
+mode=install
 if [ -d "$targetdir" ]; then
-	echo "Folder '$targetdir' already exists."
-	exit 1
+	mode=update
+	echoerr "Updating install.sh and samples in the existing '$targetdir' folder..."
 elif [ -e "$targetdir" ]; then
-	echo "File '$targetdir' already exists."
+	echoerr "File '$targetdir' already exists. Use another name."
 	exit 1
+else
+	echoerr "Creating '$targetdir' folder..."
+	mkdir "$targetdir"
 fi
 
-mkdir "$targetdir"
-cp -v $basedir/install.sh $targetdir
-for f in $basedir/*.sample; do
-	cp -v $f $targetdir
+#cp -v $basedir/install.sh $targetdir
+for f in $basedir/{install.sh,*.sample}; do
+	fbase=$(basename $f)
+	if [ -f "$targetdir/$fbase" ]; then
+		if diff -u $f $targetdir/$fbase >&2; then
+			echoerr "$fbase: unchanged."
+		else
+			echoerr "$fbase: CHAGNED."
+			cp -v $f $targetdir >&2
+		fi
+	else
+		echoerr "$fbase: NEW FILE."
+		cp -v $f $targetdir >&2
+	fi
+
+	if [ "$mode" = "install" -a "$fbase" != "install.sh" ]; then
+		dfile=$(echo -n $fbase | sed -r 's/\.sample$//')
+		cp -v $f $targetdir/$dfile >&2
+	fi
 done
 exit 0
